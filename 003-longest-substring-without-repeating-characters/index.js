a4e639982ed30653c9abc03a9e86fc46dabcee6d
hingsir <hingsir1024@gmail.com>
2016-06-21 23:20:29 +0800

    update

--- 003-longest-substring-without-repeating-characters/index.js
+++ 003-longest-substring-without-repeating-characters/index.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var longest = 0;
+
+    var longest = 0,
+        pos = -1,
+        map = {};
     for (var i = 0, len = s.length; i < len; i++) {
-        for (var j = i + 1; j <= len; j++) {
-            var sub = s.substring(i, j),
-                subLen = sub.length;
-            if (sub.indexOf(sub[subLen - 1]) === subLen - 1) {
-                if (subLen > longest) longest = subLen;
-            } else {
-                break;
-            }
+        if (map[s[i]] !== undefined && pos < map[s[i]]) {
+            pos = map[s[i]]
         }
+        if (i - pos > longest) {
+            longest = i - pos
+        }
+        map[s[i]] = i
     }
     return longest;
 };
